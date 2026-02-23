89c6de1e6810900c31fab964d9af67dac5305292
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-02-23 23:22:24 +0000

    new Changes

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -1,4 +1,15 @@
 package com.qa.factory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverFactory {
+   public WebDriver driver;
+   //public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+        driver = new ChromeDriver();
+        driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+    }
 }
