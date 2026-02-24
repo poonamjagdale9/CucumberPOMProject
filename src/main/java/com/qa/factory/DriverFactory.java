5228da358a91de5e531b66eeb1b7a5a6e4197509
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-02-24 21:52:35 +0000

    new Changes

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -5,11 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverFactory {
    public WebDriver driver;
-   //public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+   public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
 
-    public void setDriver(WebDriver driver) {
-        this.driver = driver;
+    public void init_Driver(String browser) {
+
+        System.out.println("browser value is: "+ browser);
+
+        if(browser.equals("Chrome"))
+       /* this.driver = driver;
         driver = new ChromeDriver();
-        driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+        driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");*/
     }
 }
